98f2760e499d3bb60b5a0f5d9c860cda324b048b
Julian <julian.palmanshofer@gmail.com>
2017-08-03 01:19:34 +0200

    improve cli on startup fixes #20

--- src/server.js
+++ src/server.js
@@ -2,5 +2,17 @@ import utils from './utils';
 import config from './config/config';
 
 utils.createApp().listen(config.server.port.http, function() {
-	console.log("Server started");
+	let cli =
+	"               _         _   _____ _     _       " + "\n" + 
+	" ___ ___ ___  |_|___ ___| |_|  _  | |___| |_ ___ " + "\n" +
+	"| . |  _| . | | | -_|  _|  _|     | | . |   | .'|" + "\n" +
+	"|  _|_| |___|_| |___|___|_| |__|__|_|  _|_|_|__,|" + "\n" +
+	"|_|         |___|                   |_|          " + "\n" +
+	" ------------------------------------------------" + "\n" +
+	"|               webservice started               |" + "\n" +
+	"|                   port: " + config.server.port.http + "                   |"+ "\n" +
+	"|        startup timestamp: " + Date.now() + "        |"  + "\n" +
+	" ------------------------------------------------"
+
+	console.log(cli);
 });
